b8dc7cc7f48b0d19a8d48524a6445880e4db4ae0
Msk310 <vegetableconmori@gmail.com>
2021-09-05 01:55:31 +0900

    Improved string reception

--- MegaFire_pid/MegaFire_pid.h
+++ MegaFire_pid/MegaFire_pid.h
@@ -1,14 +1,15 @@
 #include <math.h>
-#include <SPI.h>
 #include <SD.h>
 #include <Wire.h> //Needed for I2C to BMW280
 #include <MsTimer2.h>
 #include <SparkFunBME280.h>
 #include <avr/wdt.h>
 #include <avr/io.h>
+#ifdef DIAPHRAM_ENABLE
 #include <Servo.h>
+#endif
 
-//pin define
+////ピン番号の定義////
 #define GNSS_RST 22
 #define Servo_PWM 46
 #define IGsig 40
@@ -26,38 +27,41 @@
 #define SW1 8
 #define SW2 9
 #define SW3 10
+////////////////////
 
-
-#define O2PWMset OCR3A  //Timer3のDuty設定用レジスタ名
+////Timer3のDuty設定用レジスタ名////
+#define O2PWMset OCR3A  
 #define AirPWMset OCR3B
 #define LPGPWMset OCR3C
+///////////////////////////////////
 
+////SDカードファイル名////
 #define FILE_NAME "DataLog.txt"
 
 //////////グローバル変数の宣言//////////
-float Temp_IN=0.0,Temp_OUT=0.0;
-float Humidity_IN=0.0,Humidity_OUT=0.0;
-float Pressure_IN=0.0,Pressure_OUT=0.0;
-float Flow_data_LoRa[3]={0};
+float Temp = 0.0;
+float Humidity = 0.0;
+float Pressure = 0.0;
+float Flow_data[3]={0};
 int timecount=0, IG_count=0;
-uint8_t IG_flag=0 , Flow_flag=0 , time_flag=0 , IG_point[4]={0} , Pulse_Count = 0 , delay_count=0 , SD_flag=0;;
+uint8_t IG_repeat=0 , Flow_flag=0 , time_flag=0 , IG_point[4]={0} , Pulse_Count = 0 , delay_count=0 , SD_flag=0;
 float etmp_d = 0 , sum_d = 0; //1ステップ前の誤差, 誤差の総和
 double etmp_o = 0, sum_o = 0; //1ステップ前の誤差, 誤差の総和
 double etmp_a = 0, sum_a = 0;
 double etmp_g = 0, sum_g = 0;
-String Buffer_BME280_OUT;
-String Buffer_BME280_IN;
+String Buffer_BME280;
 String Buffer_Flow;
 String RECEVE_Str;
+String RECEVE_Str_LoRa;
 ////////////////////////////////////
 
 /////////////クラスの宣言/////////////
-BME280 BME280_OUT;
-BME280 BME280_IN;
+BME280 bme280;
 File myFile;
+#ifdef DIAPHRAM_ENABLE
 Servo Servo_Diaphragm;
+#endif
 ///////////////////////////////////
-
 
 ////////////関数の宣言//////////////
 void pinSetup(void);
@@ -73,14 +77,13 @@ void Air_Control();
 void LPG_Control();
 void IG_Get();
 void IG_Get2();
-void Pressure_IG();
 void IG_Pulse();
 /////////////////////////////////
 
 /****************************************************
   　　　　　　　プロトタイプ宣言した関数
  ****************************************************/
-void pinSetup(){
+void pinSetup(){    //IOピンの設定
   pinMode(IGsig, INPUT);
   pinMode(Servo_PWM,OUTPUT);
   pinMode(O2_PWM,OUTPUT);
@@ -123,7 +126,7 @@ void SDWriteData(void) {
   if (myFile) {               // if the file opened okay, write to it:
     myFile.print(millis());
     myFile.write(',');
-    myFile.print(Buffer_BME280_IN);
+    myFile.print(Buffer_BME280);
     myFile.write(',');
     myFile.print(Buffer_Flow);
   }
@@ -132,26 +135,26 @@ void SDWriteData(void) {
 
 ///////////////////////////BME280////////////////////////////////
 void setupBME280(void) {
-  BME280_OUT.setI2CAddress(0x77);
-  if (BME280_OUT.beginI2C()) {
+  bme280.setI2CAddress(0x77);
+  if (bme280.beginI2C()) {
     Wire.beginTransmission(0x77);
     Wire.write(0xf4);
     Wire.write(0x93);
     Wire.endTransmission();
     #ifdef DEBUG_SENS
-    Serial.println("BME280 Outside OK");
+    Serial.println("BME280 adress 0x77 OK");
     #endif
-    //return;
+    return;
     delay(50);
   }
-  BME280_IN.setI2CAddress(0x76);
-  if (BME280_IN.beginI2C()) {
+  bme280.setI2CAddress(0x76);
+  if (bme280.beginI2C()) {
     Wire.beginTransmission(0x76);
     Wire.write(0xf4);
     Wire.write(0x93);
     Wire.endTransmission();
     #ifdef DEBUG_SENS
-    Serial.println("BME280 Inside OK");
+    Serial.println("BME280 adress 0x76 OK");
     #endif
     return;
   }
@@ -160,134 +163,114 @@ void setupBME280(void) {
   #endif
 }
 
-void BME280_OUT_data(void) {
-  Temp_OUT = BME280_OUT.readTempC(); //°C
-  Humidity_OUT = BME280_OUT.readFloatHumidity(); //%
-  Pressure_OUT = BME280_OUT.readFloatPressure() / 100; //hPa
-}
-
-void BME280_IN_data(void){
-  Temp_IN = BME280_IN.readTempC(); //°C
-  Humidity_IN = BME280_IN.readFloatHumidity(); //%
-  Pressure_IN = BME280_IN.readFloatPressure() / 100; //hPa
+void BME280_data(void) {
+  Temp = bme280.readTempC(); //°C
+  Humidity = bme280.readFloatHumidity(); //%
+  Pressure = bme280.readFloatPressure() / 100; //hPa
 }
 
 //////////////////////////////////////////////////////////////////
 
 ///////////////////////////Buffer/////////////////////////////////
-void Create_Buffer_BME280_OUT(void){
- // Buffer.concat("Latitude,Longitude,Height\n"); 
-  Buffer_BME280_OUT.remove(0);
-  Buffer_BME280_OUT.concat(Temp_OUT);
-  Buffer_BME280_OUT.concat(","); 
-  Buffer_BME280_OUT.concat(Humidity_OUT);
-  Buffer_BME280_OUT.concat(",");
-  Buffer_BME280_OUT.concat(Pressure_OUT);
-  Buffer_BME280_OUT.concat(",");
-}
-
-void Create_Buffer_BME280_IN(void){
-  Buffer_BME280_IN.remove(0);
-//  Buffer_BME280_IN.concat(Temp_IN);
-//  Buffer_BME280_IN.concat(","); 
-//  Buffer_BME280_IN.concat(Humidity_IN);
-//  Buffer_BME280_IN.concat(",");
-  Buffer_BME280_IN.concat(Pressure_IN);
-  Buffer_BME280_IN.concat(",");
+void Create_Buffer_BME280(void){
+  Buffer_BME280.remove(0);
+  Buffer_BME280.concat(Temp);
+  Buffer_BME280.concat(","); 
+  Buffer_BME280.concat(Humidity);
+  Buffer_BME280.concat(",");
+  Buffer_BME280.concat(Pressure);
+  Buffer_BME280.concat(",");
 }
 
 void Create_Buffer_Flow(){
   Buffer_Flow.remove(0);
-  Buffer_Flow.concat(Flow_data_LoRa[0]);
+  Buffer_Flow.concat(Flow_data[0]);
   Buffer_Flow.concat(","); 
-  Buffer_Flow.concat(Flow_data_LoRa[1]);
+  Buffer_Flow.concat(Flow_data[1]);
   Buffer_Flow.concat(",");
-  Buffer_Flow.concat(Flow_data_LoRa[2]);
+  Buffer_Flow.concat(Flow_data[2]);
 }
-
-void IG_Get(int ig_time){
-  if(Serial.available()>5){
-     #ifdef DEBUG_SENS
-       Serial.println("available");
-     #endif
-    do{
-      RECEVE_Str.concat(char(Serial.read()));
-    }while(Serial.available()>0);
-    #ifdef DEBUG_SENS
-    Serial.println(RECEVE_Str);
-    #endif
-    if(RECEVE_Str.compareTo("REIG\r\n") == 0){
-       IG_flag = 1;
-       Flow_flag = !Flow_flag;
-       IG_count = ig_time;
-       delay_count = IG_TIME_DELAY;
-       #ifdef DEBUG_SENS
-       Serial.println("get REIG");
-       #endif
-      }
-      RECEVE_Str.remove(0);
-    }
-}
-
-void IG_Get2(int ig_time){
-  if(Serial2.available()>5){
-     #ifdef DEBUG_SENS
-       Serial.println("available");
-     #endif
-    do{
-      RECEVE_Str.concat(char(Serial2.read()));
-    }while(Serial2.available()>0);
-    #ifdef DEBUG_SENS
-    Serial.println(RECEVE_Str);
-    #endif
-    if(RECEVE_Str.compareTo("REIG\r\n") == 0){
-       IG_flag = 1;
-       Flow_flag = !Flow_flag;
-       IG_count = ig_time;
-       delay_count = IG_TIME_DELAY;
-       #ifdef DEBUG_SENS
-       Serial.println("get REIG");
-       #endif
-      }
-      RECEVE_Str.remove(0);
-    }
-}
-
-/*void Pressure_IG(){
-  if(Pressure_OUT < 1.0) {
-    
-  }
-  else if(Pressure_OUT < 490.0 && IG_point[0]==0){  //5km
-    IG_point[0] = 1;
-    IG_Pulse();
-  }
-  else if(Pressure_OUT < 240.0 && IG_point[1]==0){   //10km
-     IG_point[1] = 1;
-     IG_Pulse();
-  }
-  else if(Pressure_OUT < 115.0 && IG_point[2]==0){   //15km
-     IG_point[2] = 1;
-     IG_Pulse();
-  }
-  else if(Pressure_OUT < 57.0 && IG_point[3]==0){    //20km
-     IG_point[3] = 1;
-     IG_Pulse();
-  }
-}*/
-
-void IG_Pulse(){
-   IG_flag = 1;
-   Flow_flag = 1;
-   IG_count = IG_TIME+IG_TIME_DELAY;
-   Pulse_Count = FLOW_TIME*20 ;
-}
-
 //////////////////////////////////////////////////////////////////
 
-void change_freq1(int divide){    //PWMの設定をする関数 AVR.hで定義されているレジスタに直接設定を書き込む Timer1はイグナイタ、Timer3は流量制御に使用
+////////////////////////イグナイタ関係/////////////////////////////
+void IG_Get(){
+  char receve_tmp = 0;
+  while(Serial.available()>0){
+      receve_tmp = Serial.read();
+      RECEVE_Str.concat(receve_tmp);
+  }
+  if(receve_tmp == '\n'){
+      #ifdef DEBUG_SENS
+      Serial.println("available");
+      Serial.println(RECEVE_Str);
+      #endif
+      if(RECEVE_Str.compareTo("REIG\r\n") == 0){
+        IG_repeat = IG_REPEAT;
+        Flow_flag = !Flow_flag;
+        IG_count = IG_TIME;
+        delay_count = IG_TIME_DELAY;
+        #ifdef DEBUG_SENS
+        Serial.println("get REIG");
+        #endif
+      }
+      else if(RECEVE_Str.compareTo("RESET\r\n") == 0){
+        digitalWrite(RST_mega,HIGH);
+      }
+      RECEVE_Str.remove(0);
+   }
+}
+
+void IG_Get_LoRa(){
+  char receve_tmp = 0;
+  while(Serial2.available()>0){
+      receve_tmp = Serial2.read();
+      RECEVE_Str_LoRa.concat(receve_tmp);
+  }
+  if(receve_tmp == '\n'){
+      #ifdef DEBUG_SENS
+      Serial.println("available");
+      Serial.println(RECEVE_Str);
+      #endif
+      if(RECEVE_Str_LoRa.compareTo("REIG\r\n") == 0){
+        IG_repeat = IG_REPEAT;
+        Flow_flag = !Flow_flag;
+        IG_count = IG_TIME;
+        delay_count = IG_TIME_DELAY;
+        #ifdef DEBUG_SENS
+        Serial.println("get REIG");
+        #endif
+      }
+      else if(RECEVE_Str_LoRa.compareTo("RESET\r\n") == 0){
+        digitalWrite(RST_mega,HIGH);
+      }
+      RECEVE_Str_LoRa.remove(0);
+  }
+}
+
+void IG_Pulse(){
+  if(IG_repeat>0){
+    if(delay_count <1){
+      IG_count--;
+      analogWrite(IGPWM,30);
+    }
+    else delay_count--;
+    if(IG_count<1){
+      IG_repeat--;
+      IG_count = IG_TIME;
+      delay_count = IG_TIME_DELAY;
+      analogWrite(IGPWM,0);
+      }
+  }
+  else analogWrite(IGPWM,0);
+}
+//////////////////////////////////////////////////////////////////
+
+////////////////////////PWM関係の設定//////////////////////////////
+void PWM_setup(int divide){    //PWMの設定をする関数 AVR.hで定義されているレジスタに直接設定を書き込む Timer1はイグナイタ、Timer3は流量制御に使用
   //TCCR1A = (TCCR1A & B00111100) | B10000010;   //Phase and frequency correct, Non-inverting mode, TOP defined by ICR
   TCCR3A = (TCCR3A & B00000000) | B10101010;   //Timer3のモード設定
   TCCR1B = (TCCR1B & 0b11111000) | divide;     //Timer1のプリスケーラ設定
   TCCR3B = (TCCR3B & B11100000) | B00010001;   //Timer3のプリスケーラ設定
   ICR3 = 4095;                                 //Timer3の分解能設定(4095=12bit)
 }
+//////////////////////////////////////////////////////////////////
--- MegaFire_pid/MegaFire_pid.ino
+++ MegaFire_pid/MegaFire_pid.ino
@@ -1,7 +1,8 @@
- #define DEBUG_PRESS   //気圧制御のデバッグ用
+#define DEBUG_PRESS   //気圧制御のデバッグ用
 #define DEBUG_FLOW  //流量系統のデバッグ
 // #define DEBUG_FLOW_LORA //LoRa越しの流量デバッグ
 #define DEBUG_SENS  //センサ系のデバッグ用
+#define DIAPHRAM_ENABLE //ダイアフラム制御の可否
 
 //////////制御定数定義/////////////
 //各制御の目標値
@@ -47,163 +48,110 @@ const int sum_min = -5;
 #define u_d_max 105
 #define u_d_min 37
 #define Servo_INVERT 127
+////////////////////////////////////
 
-//ダイアフラム制御周期
-#define D_COUNT 1 //×Ts(ms)
-/////////////////////////////////////
-
-//////////////通信系定数//////////////
-#define IG_TIME 20 //イグナイタ点火時間
-#define IG_TIME_DELAY 40 //イグナイタの点火遅れ時間(先に燃料噴射)
+//////////////時間定数//////////////
+#define IG_TIME 10 //イグナイタ点火時間
+#define IG_TIME_DELAY 10 //イグナイタの点火遅れ時間(先に燃料噴射)
+#define IG_REPEAT 3   //イグナイタ動作の繰り返し回数
 #define Ts 50 //(ms)タイマ割り込みの周期, 制御周期
 #define SENDTIME 4  //送信間隔(s)
 #define FLOW_TIME 20
-/////////////////////////////////////
+////////////////////////////////////
 
 #include "MegaFire_pid.h"  //ライブラリとピン定義
 
 void setup(){
-  pinSetup();            //IOピンの設定
-  change_freq1(2);       //PWMの周期変更31.37kHz
-  analogWrite(IGPWM,0);
   wdt_enable(WDTO_4S);   //8秒周期のウォッチドッグタイマ開始
+  pinSetup();            //IOピンの設定
+  PWM_setup(2);          //PWMの周期変更31.37kHz
+  analogWrite(IGPWM,0);
   Serial.begin(9600);    //PCとの通信開始
-  Serial.println("MegaFire_pid");
+  Serial.println("MegaFire_pid start!");
   delay(2000);
   wdt_reset();
   delay(2000);
   wdt_reset();
-  Serial2.begin(115200);
-  Serial2.println("MegaFire");
-  while(Serial2.available()<4);
-  while(Serial2.available()>0){
-    Serial2.read();
-  }
+  Serial2.begin(115200);  //LoRaとの通信開始
+  Serial2.println("MegaFire start!");
+  while(Serial2.read() != '\n');
+  Serial.println("LoRa is Rady");
   Wire.begin();          //I2C通信開始
   setupBME280();
   SDsetup();
+  #ifdef DIAPHRAM_ENABLE
   Servo_Diaphragm.attach(Servo_PWM);
-  MsTimer2::set(Ts, TIME_Interrupt); // TsごとTIME_Interruptを呼び出す
+  #endif
+  MsTimer2::set(Ts, TIME_Interrupt); // TsごとにTIME_Interruptを呼び出す
   MsTimer2::start();
 }
 
 void loop(){
-//  BME280_OUT_data();
-if(digitalRead(SW1)==1){
-  while(digitalRead(SW1)==1);
-  SD_flag = !SD_flag;
-  Serial.print("Log ");
-  Serial.print(SD_flag);
-  Serial.println();
-}
-
-if(SD_flag==1){
-  BME280_IN_data();
-//  Create_Buffer_BME280_OUT();
-  Create_Buffer_BME280_IN();
-  SDWriteData();
-  myFile.println();
-  myFile.flush(); 
-}
-//  Serial.println("done");
-  IG_Get(IG_TIME);
-  IG_Get2(IG_TIME);
-/*if(Serial2.available()>0){
-  Serial.print((char)Serial2.read());
-}*/
-  /*if(time_flag!=0){
-      time_flag=0;*/
-    // if(timecount > (int)(SENDTIME*1000/Ts)){
-    //   Serial_print();
-    //   RECEVE_Str.remove(0);
-    //   /*if((IG_flag != 1)|| (Pressure_OUT<310.0&&Pressure_OUT>1.0&&IG_count<1)){
-    //     Serial.write(",IG");
-    //     myFile.write(",IG");
-    //   }*/
-    //   Serial.write(',');
-    //   if(analogRead(Thermocouple_PIN)>250&&Flow_flag==1)  Serial.write('2');
-    //   else Serial.print(Flow_flag);
-    //   Serial.println(); 
-    //   timecount=0;
-    // }
-//  }
-  // Pressure_IG();
-  
+  if(digitalRead(SW1)==1){
+    while(digitalRead(SW1)==1);
+    SD_flag = !SD_flag;
+    Serial.print("Log ");
+    Serial.print(SD_flag);
+    Serial.println();
+  }
+  if(SD_flag==1){
+    BME280_data();
+    Create_Buffer_BME280();
+    SDWriteData();
+    myFile.println();
+    myFile.flush(); 
+  }
+  IG_Get();
+  IG_Get_LoRa();
 }
 
 ///////////////////////サブ関数////////////////////////////
 void TIME_Interrupt(void){
-  static uint8_t Diaphram_count=0;
   wdt_reset();
   timecount++;
   if(timecount>(int)(1000/Ts)) time_flag=1;
-  //  if(Diaphram_count>D_COUNT){
-  //    sei();
-  //    BME280_OUT_data();
-  //    BME280_IN_data();
-  //    //Pressure_IN = BME280_IN.readFloatPressure() / 100; //hPa
-  //    cli();
-  //    Diaphragm_control();
-  //    Diaphram_count=0;
-  //  }
-   else Diaphram_count++;
   
   if(Flow_flag==1){
     O2_Control();
     Air_Control();
     LPG_Control();
-    Create_Buffer_Flow();
+    if(SD_flag == 1) Create_Buffer_Flow(); //ロギング中ならば流量を保存
   }
   else {
     O2PWMset=0;
     AirPWMset=0;
     LPGPWMset=0;
-    etmp_o = 0;   //1ステップ前の誤差, 誤差の総和
     sum_o = 0;
-    etmp_a = 0;
     sum_a = 0;
-    etmp_g = 0;
     sum_g = 0;
-    Flow_data_LoRa[0] = 0;
-    Flow_data_LoRa[1] = 0;
-    Flow_data_LoRa[2] = 0;
+    Flow_data[0] = 0;
+    Flow_data[1] = 0;
+    Flow_data[2] = 0;
   }
   
-  if(IG_flag==1){
-    if(delay_count <1){
-      IG_count--;
-      analogWrite(IGPWM,30);
-    }
-    else delay_count--;
-    if(IG_count<1){
-      IG_flag=0;
-      analogWrite(IGPWM,0);
-      }
-  }
-  // if(Pulse_Count>0){
-  //   Pulse_Count--;
-  //   if(Pulse_Count<2) Flow_flag=0;
-  // }
+  IG_Pulse(); //イグナイタの動作
+
+  #ifdef DIAPHRAM_ENABLE
+    sei();
+    BME280_data();
+    Diaphragm_control();
+  #endif
 }
 
 void Serial_print(void){
-  Serial.print(Buffer_BME280_OUT);
-  //Serial.write(',');
+  Serial.print(Buffer_BME280);
   #ifdef DEBUG_FLOW_LORA
-  Serial.print(Flow_data_LoRa[0]);
-  Serial.write(',');
-  Serial.print(Flow_data_LoRa[1]);
-  Serial.write(',');
-  Serial.print(Flow_data_LoRa[2]); 
+  Serial.println(Buffer_Flow);
   #endif
 }
 ///////////////////////////////////////////////////////////
 
 //////////////////////PID制御関数///////////////////////////
+#ifdef DIAPHRAM_ENABLE
 void Diaphragm_control(){
   /* 変数設定 */
   float u_d = 0;
-  float y_d = Pressure_IN; //現在の圧力
+  float y_d = Pressure; //現在の圧力
   float e_d = r_d - y_d; //誤差
   /* 制御計算 */
   // u_d = OffSet_d - (Kp_d * e_d + Ki_d * sum_d + Kd_d * (e_d - etmp_d) / (Ts * 1e-3)); //制御入力を計算
@@ -223,6 +171,7 @@ void Diaphragm_control(){
   Serial.println(u_d);
   #endif
 }
+#endif
 
 void O2_Control(){
   /* 変数設定 */
@@ -241,7 +190,7 @@ void O2_Control(){
   else if(u < 0) u = 0;
   /* 入力 */
   O2PWMset = u; //O2 PWM
-  Flow_data_LoRa[0] = y;
+  Flow_data[0] = y;
   #ifdef DEBUG_FLOW
   Serial.print("O2=");
   Serial.print(y);
@@ -268,7 +217,7 @@ void Air_Control(){
   else if(u < 0) u = 0;
   /* 入力 */
   AirPWMset = u;//Air PWM
-  Flow_data_LoRa[1] = y;
+  Flow_data[1] = y;
   #ifdef DEBUG_FLOW
   Serial.print("Air=");
   Serial.print(y);
@@ -295,7 +244,7 @@ void LPG_Control(){
   else if(u < 0) u = 0;
   /* 入力 */
   LPGPWMset = u;//LPG PWM
-  Flow_data_LoRa[2] = y;
+  Flow_data[2] = y;
   #ifdef DEBUG_FLOW
   Serial.print("LPG=");
   Serial.print(y);
